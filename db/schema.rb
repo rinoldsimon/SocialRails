3e0eb792a34411d74cbce60f6a478f4277ce803c
rinoldsimon <crisrinold@gmail.com>
2014-09-17 22:32:34 +0530

    post comment associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917160527) do
+ActiveRecord::Schema.define(version: 20140917164625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "contacts", force: true do |t|
     t.string   "name"
