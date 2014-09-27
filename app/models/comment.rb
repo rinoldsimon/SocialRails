512ef8dadf871708d4cb9ae376e44fa6dc1ea396
rinoldsimon <crisrinold@gmail.com>
2014-09-28 01:12:00 +0530

    activities modified, likes for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,8 @@ class Comment < ActiveRecord::Base
 	belongs_to :post
 	belongs_to :user
 
-	include PublicActivity::Common
-	# tracked except: :update, owner: ->(controller, model) { controller && controller.current_user }
+	include PublicActivity::Model
+	tracked owner: ->(controller, model) { controller && controller.current_user }
 	 
 	acts_as_likeable
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,5 @@
 class Like < Socialization::ActiveRecordStores::Like
+	include PublicActivity::Model
+	#tracked
+	tracked owner: ->(controller, model) { controller && controller.current_user }
 end
