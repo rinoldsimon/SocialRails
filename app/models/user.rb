507a846cd6ed6a2683374b5a560a1797e2bcb412
rinoldsimon <crisrinold@gmail.com>
2015-03-01 13:27:00 +0530

    authentication using github

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,4 +54,23 @@ class User < ActiveRecord::Base
     end
   end
 
+  devise :omniauthable, :omniauth_providers => [:github]
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name # assuming the user model has a name
+      user.remote_avatar_url = auth.info.image.gsub('http://','https://') # assuming the user model has an image 
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+        user.name = data["name"] if user.name.blank?
+      end
+    end
+  end
+
 end
