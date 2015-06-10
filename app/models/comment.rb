f64a4b7db243b18e83217d4219b5266ac352c818
rinoldsimon <crisrinold@gmail.com>
2015-06-10 13:38:15 +0530

    authorization

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
+	resourcify
+	
 	belongs_to :post
 	belongs_to :user
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+	resourcify
+	
 	belongs_to :user
 	has_many :comments, dependent: :destroy
 
